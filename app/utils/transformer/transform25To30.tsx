82b47db2dc9f837ba06d4db50d89d4448605f467
RichardIshimwe <ishimwerichard26@gmail.com>
2024-11-22 20:48:32 +0200

    progress on transfomer ui and migration

--- app/utils/transformer/transform25To30.tsx
+++ app/utils/transformer/transform25To30.tsx
@@ -5,7 +5,7 @@ export const Transform25To30 = (json25: Form25) => {
     console.log("inputed json in transformer : ",json25);
     try {
         return {
-            fields: json25.fields.map((section: Section) => transformField(section.fieldGroup[0] as AllFields)),
+            fields: json25.fields.map((section: Section) => section.fieldGroup.map((field) =>  transformField(field as AllFields))),
         };
     } catch {
         throw new Error(`Invalid 2.5 JSON`);
